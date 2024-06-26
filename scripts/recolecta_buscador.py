b7473d3b0ce6836b21fcfbeae142dfba2f428297
José M. Zavala González <jose.m.zavala.gonzalez@gmail.com>
2024-06-26 15:59:53 -0400

    Initial database workflow

--- scripts/recolecta_buscador.py
+++ scripts/recolecta_buscador.py
@@ -1,19 +1,28 @@
 import requests
 import datetime
 import pytz
+import json
+from utils_database import scaffold_database, insert_recolecta_buscador_request, \
+    insert_recolecta_buscador_records
 
+db_path = 'recolecta_corporaciones.db'
 buscador_url = 'https://rceapi.estado.pr.gov/api/corporation/search'
 
-def get_buscador_payload():
+def get_buscador_payload(limit:int = 1000):
+
+    if (limit < 1) or (limit > 1000):
+        raise ValueError('El límite debe estar entre 1 y 1000')
+    
     payload = {
         'advanceSearch': None,
         'cancellationMode': False,
         'isWorkFlowSearch': False,
-        'limit': 1000, # limite de 1000
-        'matchType': 4, # what is this?
+        'limit': limit,
         'method': None,
         'onlyActive': False,
 
+        # Default case: no busca por nombre de corporación
+        'matchType': 4, # varia
         'corpName': None, # also a parameter
 
         # Default case: no busca por número de registro
@@ -22,6 +31,28 @@ def get_buscador_payload():
     }
 
     return payload
+
+# match types:
+# 1 - Comenzando con
+# 2 - Pareo exacto
+# 3 - Cualquier palabra
+# 4 - Todas las palabras
+def get_match(match_type_description: str, corp_name: str):
+    match = {'corpName': corp_name}
+
+    if match_type_description == 'Comenzando con':
+        match['matchType'] = 1
+    elif match_type_description == 'Pareo exacto':
+        match['matchType'] = 2
+    elif match_type_description == 'Cualquier palabra':
+        match['matchType'] = 3
+    elif match_type_description == 'Todas las palabras':
+        match['matchType'] = 4
+    else:
+        raise ValueError(f'No se reconoce el tipo de búsqueda: "{match_type_description}"')
+    
+    return match
+
 
 # comparison types:
 # 1 - Es igual a
@@ -47,11 +78,6 @@ def get_comparison(comparison_type_description: str, registry_number: int):
     
     return comparison
 
-# Post request
-payload = get_buscador_payload()
-comparison = get_comparison('Mayor que', 0)
-payload.update(comparison)
-
 def get_puerto_rico_timestamp(verbose=False):
     system_now = datetime.datetime.now().astimezone()
     desired_timezone = pytz.timezone('America/Puerto_Rico')
@@ -67,9 +93,34 @@ def get_puerto_rico_timestamp(verbose=False):
         print('Desired now:', desired_now)
         print('Timestamp:', timestamp)
 
+    return timestamp
+
+
+# Ready database
+scaffold_database(db_path)
+
+
+# Post request
+payload = get_buscador_payload()
+comparison = get_comparison('Mayor que', 0)
+payload.update(comparison)
+
+timestamp = get_puerto_rico_timestamp()
+print('Timestamp:', timestamp)
+r = requests.post(buscador_url, json=payload)
+print(r.status_code)
+r.raise_for_status()
+
+request_id = insert_recolecta_buscador_request(db_path, timestamp)
+print('Request ID:', request_id)
 
 
 
-# r = requests.post(buscador_url, json=payload)
-# print(r.status_code)
+# Save to file
+json_response = r.json()
+with open('buscador_response.json', 'w') as f:
+    json.dump(json_response, f)
+
+records = json_response['response']['records']
+insert_recolecta_buscador_records(db_path, request_id, records)
 # print(r.json())
---
+++ scripts/utils_database.py
@@ -0,0 +1,102 @@
+import sqlite3
+
+def scaffold_database(db_path):
+    conn = sqlite3.connect(db_path)
+    c = conn.cursor()
+    c.execute('''
+        CREATE TABLE IF NOT EXISTS recolecta_buscador__requests (
+            request_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            request_sent_timestamp TEXT NOT NULL
+        ) STRICT;
+    ''')
+
+    {
+        "businessEntityId": 702452,
+        "registrationNumber": 1,
+        "registrationIndex": "1-811",
+        "corpName": "CENTRAL RADIOLOGY SERVICES LLP",
+        "classEs": "Soc. Resp. Limitada",
+        "classEn": "L.L.P.",
+        "profitTypeEs": "Con Fines",
+        "profitTypeEn": "For Profit",
+        "statusId": 9,
+        "statusEs": "EXPIRADA",
+        "statusEn": "EXPIRED"
+      }
+    
+    c.execute('''
+        CREATE TABLE IF NOT EXISTS recolecta_buscador__records (
+            record_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            request_id INTEGER NOT NULL,
+            business_entity_id INTEGER NOT NULL,
+            registration_number INTEGER NOT NULL,
+            registration_index TEXT NOT NULL,
+            corp_name TEXT NOT NULL,
+            class_es TEXT NOT NULL,
+            class_en TEXT NOT NULL,
+            profit_type_es TEXT NOT NULL,
+            profit_type_en TEXT NOT NULL,
+            status_id INTEGER NOT NULL,
+            status_es TEXT NOT NULL,
+            status_en TEXT NOT NULL,
+            FOREIGN KEY (request_id) REFERENCES recolecta_buscador__requests(request_id)
+        ) STRICT;          
+    ''')
+
+    conn.commit()
+    conn.close()
+
+# recolecta_buscador
+def insert_recolecta_buscador_request(db_path, timestamp):
+    conn = sqlite3.connect(db_path)
+    c = conn.cursor()
+    c.execute('''
+        INSERT INTO recolecta_buscador__requests (request_sent_timestamp)
+        VALUES (?);
+    ''', (timestamp,))
+    generated_request_id = c.lastrowid
+
+    conn.commit()
+    conn.close()
+
+    return generated_request_id
+
+recolecta_buscador_record_variable_name_mapping = {
+    'businessEntityId': 'business_entity_id',
+    'registrationNumber': 'registration_number',
+    'registrationIndex': 'registration_index',
+    'corpName': 'corp_name',
+    'classEs': 'class_es',
+    'classEn': 'class_en',
+    'profitTypeEs': 'profit_type_es',
+    'profitTypeEn': 'profit_type_en',
+    'statusId': 'status_id',
+    'statusEs': 'status_es',
+    'statusEn': 'status_en'
+}
+recolecta_buscador_record_variable_name_inverse_mapping = {
+    value: key
+    for key, value in recolecta_buscador_record_variable_name_mapping.items()
+}
+def insert_recolecta_buscador_records(db_path, request_id, records):
+    columns = tuple(recolecta_buscador_record_variable_name_mapping.values())
+    original_columns = tuple(recolecta_buscador_record_variable_name_inverse_mapping[k] for k in columns)
+    print('Columns:', columns)
+
+    print('Columns string:', ', '.join(columns))
+    print('Original columns:', original_columns)
+
+    conn = sqlite3.connect(db_path)
+    c = conn.cursor()
+    data = [
+        (request_id, *tuple(r[key] for key in original_columns))
+        for r in records
+    ]
+
+    c.executemany(f'''
+        INSERT INTO recolecta_buscador__records (request_id, {', '.join(columns)})
+        VALUES ({', '.join(['?'] * (len(columns) + 1))});
+    ''', data)
+
+    conn.commit()
+    conn.close()
